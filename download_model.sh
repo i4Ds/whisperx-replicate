bc31369c110ad44a3f0f67335ced9a5e101d9621
Vincenzo <vincenzo.timmel@fhnw.ch>
2024-07-03 15:05:31 +0200

    Fixes bash script for model download.

--- download_model.sh
+++ download_model.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# MODEL PATH
+MODEL_PATH=i4ds/whisper4sg-srg-v2-full-mc-de-sg-corpus-v2
 
 # To download it to the cache, because there is no internet in the container.
 python3 -c "import os; os.environ['HF_HOME'] = 'hf_home'; from faster_whisper import WhisperModel; WhisperModel('$MODEL_PATH')"
